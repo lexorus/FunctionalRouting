c85be2f398ad7883b0907feb63d143fb9c0710d4
DmitriiCelpan <dmitrii.celpan@yopeso.com>
2016-11-24 17:24:43 +0200

    Prepare ViewControllers

---
+++ FunctionalRouting/ViewControllers/DViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DViewController.swift
+//  FunctionalRouting
+//
+//  Created by Dmitrii Celpan on 11/24/16.
+//  Copyright © 2016 Lexorus. All rights reserved.
+//
+
+import UIKit
+
+final class DViewController: UIViewController {
+    
+    // VERY COMPLICATED LOGIC (related only to this ViewController/Module)
+    
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+}
---
+++ FunctionalRouting/ViewControllers/FViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FViewController.swift
+//  FunctionalRouting
+//
+//  Created by Dmitrii Celpan on 11/24/16.
+//  Copyright © 2016 Lexorus. All rights reserved.
+//
+
+import UIKit
+
+final class FViewController: UIViewController {
+    
+    // VERY COMPLICATED LOGIC (related only to this ViewController/Module)
+    
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+}
